bc01d5f7ad76d59d1149c4722d370792fcb7a48a
IthielHat <124407444+IthielHat@users.noreply.github.com>
2023-02-03 18:14:26 -0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,27 @@
+#x = "I love to eat Clara"
+
+
+#def myfunc():
+               # global y    
+                #y = "I love potatoes"
+                #print("Me, Ithiel " + x)
+
+#myfunc()
+
+#print("Me, Ithiel " + x)
+#print (y)
+
+
+import random
+VU = random.randrange(1,100)
+print("Le chiffre obtenu est " + str(VU) )
+
+if VU > 50: 
+                PU = "Ithiel est moche"
+                print(PU)
+
+if VU < 50 :
+                DU = "Ithiel est beau"
+                print(DU) 
+
+
